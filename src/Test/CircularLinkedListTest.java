97dc866b5b802de24c9b05f8903b6f7be81a4e7b
aoflus <alvaro.ortega.rh@gmail.com>
2019-09-21 14:40:28 +0200

    Create add, get and size logic
    Created logic for the add, the indexed add, the get and the size methods
    for the circular linked list structure. Added test cases for the
    methods.

---
+++ src/Test/CircularLinkedListTest.java
@@ -0,0 +1,39 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import src.CircularLinkedList;
+
+class CircularLinkedListTest {
+
+	public List<Integer> list;
+	
+	@BeforeEach
+	public void setUp() {
+		list = new CircularLinkedList<Integer>();
+		list.add(new Integer(4));
+		list.add(new Integer(7));
+		list.add(new Integer(1));
+	}
+	
+	@Test
+	public void testAdd() {
+		list.add(new Integer(4));
+		assertTrue(list.size() == 4);
+	}
+	
+	@Test
+	public void testAddWithIndex() {
+		list.add(1, new Integer(4));
+		assertTrue(list.get(1).equals(new Integer(4)));
+		
+		list.add(-1, new Integer(20));
+		assertTrue(list.get(-1).equals(20));
+	}
+
+}
---
+++ src/src/CircularLinkedList.java
@@ -0,0 +1,211 @@
+package src;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class CircularLinkedList<E> implements List<E> {
+	
+	private int size;
+	private Node head;
+	private Node tail;
+	
+	private class Node {
+		
+		public E element;
+		public Node next;
+		
+		public Node(E element) {
+			this.element = element;
+		}
+		
+		public Node(E element, Node next) {
+			this.element = element;
+			this.next = next;
+		}
+		
+		public String toString() {
+			return "Node : " + element.toString();
+		}
+	}
+	
+	private Node getNode(int index) {
+		/* Check for indexes that loop the circular list */
+		index = checkForLoops(index);
+		
+		/* Get the node after the check */
+		if (index == 0) { // get node at 0 with O(1) complexity
+			return head;
+		} else {
+			Node node = head;
+			for (int i = 0; i < size; i++) {
+				if  (i == index) {
+					return node;
+				}
+				node = node.next;
+			}
+		}
+		return null;
+	}
+	
+	private int checkForLoops(int index) {
+		if (index < 0) {
+			if (Math.abs(index) > size) { // The negative index traverses the circular list more than once
+				int val = Math.abs(index) % size;
+				index = index-val;
+			} else {
+				index = size - Math.abs(index);
+			}
+		} else if (index >= size) {
+			index = index % size;
+		}
+		return index;
+	}
+
+	@Override
+	public boolean add(E e) {
+		if (size == 0) {
+			head = new Node(e, head);
+			tail = head;
+		} else {
+			tail.next = new Node(e, head);
+			tail = tail.next;
+		}
+		size++;
+		return true;
+	}
+
+	@Override
+	public void add(int index, E element) {
+		Node prev = getNode(index);
+		Node node = new Node(element, prev.next);
+		prev.next = node;
+		size++;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<? extends E> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E get(int index) {
+		return getNode(index).element;
+	}
+
+	@Override
+	public int indexOf(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int lastIndexOf(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ListIterator<E> listIterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ListIterator<E> listIterator(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E set(int index, E element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		return this.size;
+	}
+
+	@Override
+	public List<E> subList(int fromIndex, int toIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
